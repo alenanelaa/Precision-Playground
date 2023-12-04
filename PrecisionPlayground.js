e1b3cf005a18b6ce11c84f8515895c06ba1eeea6
Alena Zhu <alena.k.zhu@gmail.com>
2023-12-03 19:23:35 -0800

    fps perspective and crosshair

--- PrecisionPlayground.js
+++ PrecisionPlayground.js
@@ -1,10 +1,11 @@
+import FPSCam from './camera-view.js';
 import { defs, tiny } from './examples/common.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
 
-const { Cube, Axis_Arrows, Textured_Phong, Phong_Shader } = defs;
+const { Cube, Square, Subdivision_Sphere, Axis_Arrows, Textured_Phong, Phong_Shader, Basic_Shader } = defs;
 
 export class PrecisionPlayground extends Scene {
     constructor() {
@@ -12,7 +13,8 @@ export class PrecisionPlayground extends Scene {
 
         this.shapes = {
             floor: new Cube(),
-            target: new defs.Subdivision_Sphere(4)
+            target: new Subdivision_Sphere(4),
+            crosshair: new Square()
         };
 
         this.materials = {
@@ -23,58 +25,74 @@ export class PrecisionPlayground extends Scene {
             blue_sphere: new Material(new Phong_Shader(), {
                 color: hex_color("#0000ff"),
                 ambient: 1, diffusivity: 0.5, specularity: 0.5
+            }),
+            crosshair: new Material(new Textured_Phong(), {
+                color: hex_color("#000000"),
+                ambient: 1, diffusivity: 0.1, specularity: 0.1,
+                texture: new Texture("assets/crosshair.png")
             })
         };
 
-        this.flags = {
-            paused: false
-        };
+        this.paused = false; //for pause and play (may wanna change it to be game states)
 
+        this.controls = {
+            sens: 1
+        }
         this.sphere_positions = [];
         for (let i = 0; i < 6; i++) {
             this.sphere_positions.push(vec3(0, 0, 0));
         }
 
-        this.initial_camera_location = Mat4.look_at(vec3(0, 0, -22.36), vec3(0, 0, 0), vec3(0, 1, 0));
-
-
-        // Initialize controls in the constructor
-        this.make_control_panel();
-        this.children.push(this.mouse_controls = new defs.Movement_Controls());
+        this.camera = new FPSCam(0, 0, 20);
     }
 
-    //all this code doesn't even do anything because the object doesn't have a canvas element what even is the fucking point where did this come from
-    make_control_panel() {
-        this.mouse = { "from_center": vec(0, 0) };
-        
-        // Assuming this.canvas is your canvas element
-        if (this.canvas) {
-            this.mouse_controls = new defs.Movement_Controls();
-            this.add_pointer_lock();
-            this.mouse_controls.add_canvas_listener(this.canvas, (e) => this.locked_mouse_handler(e));
-        } else {
-            console.error("Canvas element not found. Make sure you have a canvas element in your HTML.");
-        }
-    }
+    shoot(pos, program_state) {
+        pos = [0,0];
 
-    
-
-    mouse_handler(event) {
-        this.mouse.from_center = vec(event.movementX, event.movementY);
-
-        const sensitivity = 0.01;
-        
-        // Rotate the camera based on mouse movements
-        const rotation = Mat4.rotation(sensitivity * this.mouse.from_center[0], vec3(0, 1, 0))
-            .times(Mat4.rotation(sensitivity * this.mouse.from_center[1], vec3(1, 0, 0)));
-
-        // Update the camera's transformation
-        program_state.camera_transform = rotation.times(program_state.camera_transform);
+        let pos_ndc_near = vec4(pos[0], pos[1], -1.0, 1.0);
+        let pos_ndc_far = vec4(pos[0], pos[1], 1.0, 1.0);
+        let center_ndc_near = vec4(0.0, 0.0, -1.0, 1.0);
+        let P = program_state.projection_transform;
+        let V = program_state.camera_inverse;
+        let pos_world_near = Mat4.inverse(P.times(V)).times(pos_ndc_near);
+        let pos_world_far = Mat4.inverse(P.times(V)).times(pos_ndc_far);
+        let center_world_near = Mat4.inverse(P.times(V)).times(center_ndc_near);
+        pos_world_near.scale_by(1/pos_world_near[3]);
+        pos_world_far.scale_by(1/pos_world_far[3]);
+        center_world_near.scale_by(1/center_world_near[3]);
     }
 
     display(context, program_state) {
+        let lookAt = this.camera.lookAt;
+        let canvas = context.canvas;
+        program_state.set_camera(lookAt);
+
         if (!context.scratchpad.controls) {
-            program_state.set_camera(this.initial_camera_location);
+            const mouse_position = (e, rect = canvas.getBoundingClientRect()) =>
+                vec(
+                    (e.clientX - (rect.left + rect.right)/2) /
+                        ((rect.right - rect.left)/2),
+                    (e.clientY - (rect.bottom + rect.top)/2) /
+                        ((rect.top - rect.bottom)/2)
+                );
+
+            //PointerLock API
+            canvas.addEventListener("mousedown", async (e) => {
+                e.preventDefault();
+                if (!document.pointerLockElement && !this.paused) {
+                    await canvas.requestPointerLock();
+                }
+                canvas.addEventListener("mousemove", (e) => {
+                    let del_x = e.movementX;
+                    let del_y = e.movementY;
+
+                    if (!this.paused) {
+                        this.camera.update_cam(del_x, del_y, this.controls.sens);
+                    }            
+
+                });
+                this.shoot(mouse_position(e), program_state);
+            }, {once: true});
         }
 
         program_state.projection_transform = Mat4.perspective(
@@ -87,10 +105,9 @@ export class PrecisionPlayground extends Scene {
         let model_transform = Mat4.identity();
 
         let floor_transform = model_transform.times(Mat4.translation(0, -5, 0)).times(Mat4.scale(20, 0.1, 20));
-
-
-
         this.shapes.floor.draw(context, program_state, floor_transform, this.materials.test);
+
+        this.camera.interface(context, program_state, this.shapes, this.materials); //drawing crosshair and eventually score
 
         // Check if two seconds have passed
         if (t % 1 < dt) {
--- camera-view.js
+++ camera-view.js
@@ -18,4 +18,20 @@ export default class FPSCam {
         this.t = 0;
         this.p = 0;
     }
+
+    update_cam(del_x, del_y, sensitivity) {
+        this.t += del_x * (sensitivity * 0.75) / 50000;
+        this.p += (-1) * del_y * (sensitivity * 0.75) / 50000;
+        this.at = vec3(this.t, this.p, 1)
+        this.lookAt = Mat4.look_at(vec3(this.x, this.y, this.z), this.at, vec3(0, 1, 0));
+        return this.lookAt;
+    }
+
+    interface(context, program_state, shapes, materials) {
+        let cam_matrix = Mat4.inverse(this.lookAt);
+
+        // crosshair
+        let ch_transform = cam_matrix.times(Mat4.translation(0, 0, -1)).times(Mat4.scale(0.02, 0.02, 0.02));
+        shapes.crosshair.draw(context, program_state, ch_transform, materials.crosshair);
+    }
 }
